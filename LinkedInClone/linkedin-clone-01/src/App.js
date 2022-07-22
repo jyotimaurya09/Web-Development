146c72ef8801c706cde762cc736b830501013f4f
jyotimaurya09 <jyoti029mtcse21@igdtuw.ac.in>
2022-07-22 20:16:07 +0530

    Linkedin Frontend

---
+++ LinkedInClone/linkedin-clone-01/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Login from './components/Login';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/home" element={<><Header /> <Home /></>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
